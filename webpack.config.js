4e60e255820d0472a960ea7abd9d2816560b3e39
pactience <1079105171@qq.com>
2020-07-21 15:50:09 +0800

    feat(搭建脚手架的基本内容): 添加react,typescript.添加typescript基本配置,webpack基本配置

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["less-loader"],
+      },
+    ],
+  },
+};
